b708c42e82d88bc7d0d8b99d20ee104132c7becf
Victor Ormeño <20100835@ue.edu.pe>
2023-06-05 02:54:03 -0500

    CSN

--- UESAN.Jobs.Infrastructure/Models/BolsaDeTrabajoContext.cs
+++ UESAN.Jobs.Infrastructure/Models/BolsaDeTrabajoContext.cs
@@ -28,7 +28,7 @@ public partial class BolsaDeTrabajoContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=LAPTOP-HP1HJ343\\SOY_YO;DataBase=Bolsa_de_trabajo;TrustServerCertificate=True;Integrated Security=true");
+        => optionsBuilder.UseSqlServer("Server=;DataBase=Bolsa_de_trabajo;TrustServerCertificate=True;Integrated Security=true");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
